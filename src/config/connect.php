c61a6618b9dad2b3abf28272a1255cb04717a5a7
davidRamirez1005 <ramirezjd1005@gmail.com>
2024-02-22 21:15:31 -0500

    feat: :sparkles: coneccion a base de datos
    se hace por  medio de PDO y se usan variables de entorno

---
+++ src/config/connect.php
@@ -0,0 +1,15 @@
+<?php
+    namespace App;
+    class Connect{
+        public $con;
+        function __construct(){
+            try {
+                $this->con = new \PDO($_ENV["DNS"].":host=".$_ENV["HOST"].";dbname=".$_ENV["DBNAME"].";user=".$_ENV["USER"].";password=".$_ENV["PASSWORD"].";port=".$_ENV["PORT"]);
+                $this->con->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT);
+                $this->con->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
+                $this->con->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            } catch (\PDOException $e) {
+                echo  $e->getMessage();
+            }
+        }
+    }
