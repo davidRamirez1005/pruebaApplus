41ca06f50b527d4838bd625d66b109f76a474ef7
davidRamirez1005 <ramirezjd1005@gmail.com>
2024-02-23 13:38:33 -0500

    feat: :sparkles: modelos y helpers

---
+++ app/helpers.php
@@ -0,0 +1,10 @@
+<?php
+
+function view($name, $data = []) {
+    extract($data);
+    require "resources/views/{$name}.view.php";
+}
+
+function redirect($path) {
+  header("Location: {$path}");
+}
